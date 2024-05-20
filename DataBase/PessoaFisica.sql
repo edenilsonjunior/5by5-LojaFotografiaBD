7a535fbf4ed8d5d5449476c4fdc8f1823f233124
Edenilson <edenilsonju10@gmail.com>
2024-05-20 09:48:57 -0300

    update das tabelas nao tinham sido feitas commit

--- DataBase/PessoaFisica.sql
+++ DataBase/PessoaFisica.sql
@@ -3,8 +3,8 @@ CREATE TABLE PessoaFisica
 	cod_cliente int not null,
 	sexo char(1) not null,
 	data_nasc date not null,
-	rg int not null,
-	cpf int not null,
+	rg int not null unique,
+	cpf int not null unique,
 	CONSTRAINT pkpessoafisica PRIMARY KEY (cod_cliente),
 	CONSTRAINT fkpessoafisicacliente FOREIGN KEY (cod_cliente) REFERENCES Cliente(codigo)
 );
--- DataBase/PessoaJuridica.sql
+++ DataBase/PessoaJuridica.sql
@@ -2,8 +2,8 @@ CREATE TABLE PessoaJuridica
 (
 	cod_cliente int not null,
 	nome_responsavel varchar(30) not null,
-	insc_estadual int not null,
-	cnpj int not null,
+	insc_estadual int not null unique,
+	cnpj int not null unique,
 	CONSTRAINT pkpessoajuridica PRIMARY KEY (cod_cliente),
 	CONSTRAINT fkpessoajuridicacliente FOREIGN KEY (cod_cliente) REFERENCES Cliente(codigo)
 );
